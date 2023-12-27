dc5152549ca92a68a1172a433d47ae1930aabdbb
Alanis Swanepoel <alanis@arya.local>
2023-12-27 07:10:13 +0000

    adding versioninfo

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dragonsecurity/breachdetector
 go 1.21.5
 
 require (
+	github.com/carlmjohnson/versioninfo v0.22.5
 	github.com/jmoiron/sqlx v1.3.5
 	github.com/lib/pq v1.10.9
 	github.com/lmittmann/tint v1.0.3
--- internal/version/version.go
+++ internal/version/version.go
@@ -2,34 +2,13 @@ package version
 
 import (
 	"fmt"
-	"runtime/debug"
+	"github.com/carlmjohnson/versioninfo"
 )
 
 func Get() string {
-	var revision string
-	var modified bool
-
-	bi, ok := debug.ReadBuildInfo()
-	if ok {
-		for _, s := range bi.Settings {
-			switch s.Key {
-			case "vcs.revision":
-				revision = s.Value
-			case "vcs.modified":
-				if s.Value == "true" {
-					modified = true
-				}
-			}
-		}
-	}
-
-	if revision == "" {
-		return "unavailable"
-	}
-
-	if modified {
-		return fmt.Sprintf("%s-dirty", revision)
-	}
-
-	return revision
+	fmt.Println("Version:", versioninfo.Version)
+	fmt.Println("Revision:", versioninfo.Revision)
+	fmt.Println("DirtyBuild:", versioninfo.DirtyBuild)
+	fmt.Println("LastCommit:", versioninfo.LastCommit)
+	return fmt.Sprintf("%s", versioninfo.Short())
 }
